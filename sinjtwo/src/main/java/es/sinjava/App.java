ba73b6c2ca61c69401bb8888a25537a4afa199c1
Andres Gaudioso <andres.serikat@outlook.es>
2018-04-14 15:34:49 +0200

    Linea al principio

--- sinjtwo/src/main/java/es/sinjava/App.java
+++ sinjtwo/src/main/java/es/sinjava/App.java
@@ -3,6 +3,7 @@ package es.sinjava;
 
 public class App {
 	public static void main(String... args) {
+		// Se introduce una nueva linea
 		System.out.println("Hola Mundo , git, con los cambios");
 	}
 }
