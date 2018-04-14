b483895c66a708eb087f8d655a8a99331df82e89
Andres Gaudioso <andres.serikat@outlook.es>
2018-04-14 15:04:04 +0200

    ASumimos los cambios

--- sinjtwo/src/main/java/es/sinjava/App.java
+++ sinjtwo/src/main/java/es/sinjava/App.java
@@ -3,6 +3,6 @@ package es.sinjava;
 
 public class App {
 	public static void main(String... args) {
-		System.out.println("Hola Mundo , git");
+		System.out.println("Hola Mundo , git, con los cambios");
 	}
 }
