9189baab6965b735523e9c281a6f2d084ef896a7
Andres Gaudioso <andres.serikat@outlook.es>
2018-04-14 15:41:47 +0200

    Se modifica una linea de otro

--- sinjtwo/src/main/java/es/sinjava/App.java
+++ sinjtwo/src/main/java/es/sinjava/App.java
@@ -5,7 +5,7 @@ public class App {
 	public static void main(String... args) {
 		// Se introduce una nueva linea
 		System.out.println("Hola Mundo , git, con los cambios");
-		// añadida una nueva linea
+		// añadida una nueva linea y modificada la segunda
 
 	}
 }
