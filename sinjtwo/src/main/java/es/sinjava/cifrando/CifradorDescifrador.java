35b566b964facd118f76fe7b54c8912ec8fb0ad4
andre_k <andre_k@DESKTOP10.home>
2018-04-23 16:32:26 +0200

    Atacando el almacen de certificados de java

--- sinjtwo/src/main/java/es/sinjava/cifrando/CifradorDescifrador.java
+++ sinjtwo/src/main/java/es/sinjava/cifrando/CifradorDescifrador.java
@@ -1,12 +1,17 @@
 package es.sinjava.cifrando;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.security.GeneralSecurityException;
 import java.security.KeyFactory;
+import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.cert.Certificate;
+import java.security.interfaces.RSAPrivateCrtKey;
 import java.security.spec.KeySpec;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
@@ -17,6 +22,7 @@ public class CifradorDescifrador {
 
 	private static Cipher cipherRsa;
 	private static KeyFactory keyFactory;
+	private static KeyStore ks;
 
 	public static CifradorDescifrador getInstanceRsa() {
 		try {
@@ -26,6 +32,24 @@ public class CifradorDescifrador {
 			System.out.println("Error: " + e);
 		}
 		return new CifradorDescifrador();
+	}
+
+	public byte[] encriptCacert(String alias, char[] password, byte[] textAsByte)
+			throws IOException, GeneralSecurityException {
+		PublicKey publicKey = loadPublicKeyFromCacert(alias, password);
+		cipherRsa.init(Cipher.ENCRYPT_MODE, publicKey);
+		byte[] encriptado = cipherRsa.doFinal(textAsByte);
+
+		return encriptado;
+	}
+
+	public byte[] deEncriptCacert(String alias, char[] password, byte[] textAsByte)
+			throws IOException, GeneralSecurityException {
+		PrivateKey privateKey = loadPrivateKeyFromCacert(alias, password);
+		cipherRsa.init(Cipher.DECRYPT_MODE, privateKey);
+		byte[] bytesDesencriptados = cipherRsa.doFinal(textAsByte);
+
+		return bytesDesencriptados;
 	}
 
 	public byte[] encript(String publicKeyFile, byte[] textAsByte) throws IOException, GeneralSecurityException {
@@ -57,6 +81,28 @@ public class CifradorDescifrador {
 		KeySpec keySpec = new PKCS8EncodedKeySpec(content);
 		PrivateKey keyFromBytes = keyFactory.generatePrivate(keySpec);
 		return keyFromBytes;
+	}
+
+	private PublicKey loadPublicKeyFromCacert(String alias, char[] password)
+			throws GeneralSecurityException, IOException {
+		KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+		String filename = System.getProperty("java.home") + "/lib/security/cacerts".replace('/', File.separatorChar);
+		InputStream inputStream = new FileInputStream(filename);
+		ks.load(inputStream, password);
+		Certificate certificado = ks.getCertificate(alias);
+		inputStream.close();
+		return certificado.getPublicKey();
+	}
+
+	private PrivateKey loadPrivateKeyFromCacert(String alias, char[] password)
+			throws IOException, GeneralSecurityException {
+		KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+		String filename = System.getProperty("java.home") + "/lib/security/cacerts".replace('/', File.separatorChar);
+		InputStream inputStream = new FileInputStream(filename);
+		ks.load(inputStream, password);
+		PrivateKey key = (RSAPrivateCrtKey) ks.getKey("sinjavalbs", password);
+		inputStream.close();
+		return key;
 	}
 
 }
