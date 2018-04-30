28269ac48394315d67edf53e5789030e576fc8b3
andre_k <andre_k@DESKTOP10.home>
2018-04-30 19:01:17 +0200

    Actualización campera

--- twodatasources/src/main/java/es/sinjava/data/BaseConfig.java
+++ twodatasources/src/main/java/es/sinjava/data/BaseConfig.java
@@ -1,20 +1,16 @@
 package es.sinjava.data;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 
 import javax.ws.rs.Path;
 
 import org.apache.cxf.bus.spring.SpringBus;
 import org.apache.cxf.endpoint.Server;
-import org.apache.cxf.interceptor.Interceptor;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.apache.cxf.jaxrs.lifecycle.ResourceProvider;
 import org.apache.cxf.jaxrs.spring.JaxRsConfig;
 import org.apache.cxf.jaxrs.spring.SpringResourceFactory;
-import org.apache.cxf.message.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,11 +35,6 @@ public class BaseConfig {
 	@Autowired
 	ApplicationContext ctx;
 
-	// Metemos el interceptor en el contexto por si hay que hacer algo complejo
-	// y tal
-	@Autowired
-	private InInterceptor inInterceptor;
-
 	private static final Logger LOG = LoggerFactory.getLogger(BaseConfig.class);
 
 	@Bean
@@ -58,12 +49,10 @@ public class BaseConfig {
 
 		JAXRSServerFactoryBean jaxRSServerFactoryBean = new JAXRSServerFactoryBean();
 		jaxRSServerFactoryBean.setBus(ctx.getBean(SpringBus.class));
-		jaxRSServerFactoryBean.setProviders(Arrays.asList(new JacksonJsonProvider(), new CorsProvider()));
+		jaxRSServerFactoryBean.setProviders(Arrays.asList(new JacksonJsonProvider()));
 
 		jaxRSServerFactoryBean.setResourceProviders(resourceProviders);
-		List<Interceptor<? extends Message>> inter = new ArrayList<>();
-		inter.add(inInterceptor);
-		jaxRSServerFactoryBean.setInInterceptors(inter);
+
 		return jaxRSServerFactoryBean.create();
 	}
 }
